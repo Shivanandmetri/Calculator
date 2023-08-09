d313e003eab57d2f25d6898fde3e72773b667ba9
Shivanandmetri <shivanand1998metri@gmail.com>
2023-08-09 11:40:16 +0530

    commit

---
+++ src/Components/ButtonComp.js
@@ -0,0 +1,17 @@
+import {View, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+const ButtonComp = () => {
+  const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  return (
+    <View>
+      <TouchableOpacity>
+        <View>
+          <Text>{}</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ButtonComp;
